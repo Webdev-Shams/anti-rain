b0384106ea5c9e5f608a0d2249900795358f674c
Webdev-Shams <webdev.shams@gmail.com>
2022-06-14 13:16:39 +0600

    components folder and files added!

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Anti Rain</h1>
+            <p>Stay dry!</p>
+        </div>
+    );
+};
+
+export default Header;
